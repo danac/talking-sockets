bbbb9854a289d5d69808256cb6559d6e5070b420
Dana Christen <dana.christen@gmail.com>
2015-01-08 19:19:27 +0100

    Updated the Travis CI config and added Coveralls support

---
+++ README.rst
@@ -0,0 +1,4 @@
+.. image:: https://api.travis-ci.org/danac/talking-sockets.png
+    :target: https://travis-ci.org/danac/talking-sockets
+.. image:: https://coveralls.io/repos/danac/talking-sockets/badge.png
+    :target: https://coveralls.io/r/danac/talking-sockets
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='Talking Sockets',
+      version='1.0',
+      description='Connection routing library',
+      author='Dana Christen',
+      author_email='dana.christen@gmail.com',
+      url='https://github.com/danac/talking-sockets',
+      packages=['talking_sockets'],
+     )
