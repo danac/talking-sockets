d8d9c4fb4f470c2c84047116459ad0706a983056
Dana Christen <dana.christen@gmail.com>
2015-02-02 16:35:01 +0100

    Changed license: moved from GPLv3 to LGPLv3

--- talking_sockets/endpoints/ws_server.py
+++ talking_sockets/endpoints/ws_server.py
@@ -6,15 +6,15 @@
 # This file is part of talking-sockets, a tool for connection routing.
 #
 # talking-sockets is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# talking-sockets is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with talking-sockets. If not, see <http://www.gnu.org/licenses/>.
 #
